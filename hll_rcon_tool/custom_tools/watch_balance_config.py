0bf614d7041756f0d769d7850e7780c7159ba1c9
ElGuillermo <66552553+ElGuillermo@users.noreply.github.com>
2024-11-08 18:39:10 +0100

    Create watch_balance_config.py

---
+++ hll_rcon_tool/custom_tools/watch_balance_config.py
@@ -0,0 +1,43 @@
+"""
+watch_balance_config
+
+A plugin for HLL CRCON (https://github.com/MarechJ/hll_rcon_tool)
+that filters (kick) players based upon their language.
+
+Source : https://github.com/ElGuillermo
+
+Feel free to use/modify/distribute, as long as you keep this note in your code
+"""
+
+# Discord embeds strings translations
+# Available : 0 for english, 1 for french, 2 for german
+LANG = 0
+
+# Dedicated Discord's channel webhook
+# ServerNumber, Webhook, Enabled
+SERVER_CONFIG = [
+    ["https://discord.com/api/webhooks/...", True],  # Server 1
+    ["https://discord.com/api/webhooks/...", False],  # Server 2
+    ["https://discord.com/api/webhooks/...", False],  # Server 3
+    ["https://discord.com/api/webhooks/...", False],  # Server 4
+    ["https://discord.com/api/webhooks/...", False],  # Server 5
+    ["https://discord.com/api/webhooks/...", False],  # Server 6
+    ["https://discord.com/api/webhooks/...", False],  # Server 7
+    ["https://discord.com/api/webhooks/...", False],  # Server 8
+    ["https://discord.com/api/webhooks/...", False],  # Server 9
+    ["https://discord.com/api/webhooks/...", False]  # Server 10
+]
+
+
+# Miscellaneous (you don't have to change these)
+# ----------------------------------------------
+
+# The interval between watch turns (in seconds)
+# Recommended : as the stats must be gathered for all the players,
+#               requiring some amount of data from the game server,
+#               you may encounter slowdowns if done too frequently.
+# Default : 300
+WATCH_INTERVAL_SECS = 300
+
+# Bot name that will be displayed in CRCON "audit logs" and Discord embeds
+BOT_NAME = "CRCON_watch_balance"
